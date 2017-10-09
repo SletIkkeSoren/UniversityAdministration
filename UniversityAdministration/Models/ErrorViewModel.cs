f9401b38e048d92c95dd3c14c219df52e19f6378
Søren <buraktekelioglu17@gmail.com>
2017-10-09 09:35:36 +0200

    first commit

---
+++ UniversityAdministration/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UniversityAdministration.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
